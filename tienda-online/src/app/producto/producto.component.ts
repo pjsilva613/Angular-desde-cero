7e4b41bfa384dc702dca465c9ebcaa28e8e95eef
pjsilva613 <pjsilva613@gmail.com>
2025-04-02 11:09:22 -0500

    Se realiza el reto tienda online

---
+++ tienda-online/src/app/producto/producto.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  imports: [],
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.css'
+})
+export class ProductoComponent {
+
+  descripcion = 'nuevo producto';
+  precio = 100.00;
+
+}

a409745e1d313e608934bbbc34490b321171cc95
pjsilva613 <pjsilva613@gmail.com>
2025-04-02 11:54:45 -0500

    Se realiza capitulo de event binding

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, MostrarMensajeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  titulo = 'Hola Mundo Desde Angular';
+  titulo = 'Universidad Angular';
 }

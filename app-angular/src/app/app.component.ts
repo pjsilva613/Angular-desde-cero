fd3f8d07ffa6fd0fd9e47a9ef49e79781a6f888e
pjsilva613 <pjsilva613@gmail.com>
2025-04-02 09:52:23 -0500

    Se realiza uso de varios componentes padre e hijo

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ app-angular/src/app/padre/hijo/hijo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  imports: [],
+  templateUrl: './hijo.component.html',
+  styleUrl: './hijo.component.css'
+})
+export class HijoComponent {
+
+}

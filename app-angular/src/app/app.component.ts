2c26c051344a23694ac2453978381e044f8567f2
pjsilva613 <pjsilva613@gmail.com>
2025-04-04 13:21:19 -0500

    Se realiza leccion de property binding

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ReplicadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  titulo = 'Hola Mundo Desde Angular';
+  titulo = 'Property Binding Angular';
 }
